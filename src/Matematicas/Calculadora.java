99833279ae17d7646a45e1b3a04be3716b2146cf
usuario <usuario@pc20-051>
2020-11-05 09:14:46 +0100

    V2 codigo minimo

--- src/Matematicas/Calculadora.java
+++ src/Matematicas/Calculadora.java
@@ -1,5 +1,13 @@
 package Matematicas;
 
 public class Calculadora {
+
+    static int suma(int i, int i0) {
+        return 8;
+        }
+
+    static int resta(int i, int i0) {
+        return 8;
+    }
     
 }
