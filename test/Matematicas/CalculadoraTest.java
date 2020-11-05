b9cece0baed357f097ad3fa7645b58e79b15a46c
usuario <usuario@pc20-051>
2020-11-05 09:09:16 +0100

    V1

---
+++ test/Matematicas/CalculadoraTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Matematicas;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author usuario
+ */
+public class CalculadoraTest {
+    
+    public CalculadoraTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testSuma(){
+        int total=8;
+        int suma = Calculadora.suma(5,3);
+        assertEquals(total,suma);
+    }
+    
+    @Test
+    public void testResta(){
+        int total=8;
+        int res = Calculadora.resta(10,2);
+        assertEquals(total,res);
+    }
+    
+}
